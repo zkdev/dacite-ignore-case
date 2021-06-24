a14db170a9a8e8bbf01142ddd8916bbadf9bacc2
zkdev <dev@zeekay.dev>
2021-06-24 22:44:16 +0200

    Add long desc

--- setup.py
+++ setup.py
@@ -1,19 +1,33 @@
+import os
+
 import setuptools
+
+
+own_dir = os.path.abspath(os.path.dirname(__file__))
+
+
+def readme():
+    with open(os.path.join(own_dir, 'README.md')) as f:
+        for line in f.readlines():
+            line = line.strip()
+            yield line
 
 
 setuptools.setup(
     name='dacite_ignore_case',
-    version='0.0.1',
+    version='0.0.2',
     description='Extends dacite.from_dict with a ignore case function',
     author='zkdev',
     author_email='dev@zeekay.dev',
+    long_description = readme(),
+    long_description_content_type = 'text/markdown',
     project_urls={
-        "Bug Tracker": "https://github.com/zkdev/dacite-ignore-case/issues",
+        'Bug Tracker': 'https://github.com/zkdev/dacite-ignore-case/issues',
     },
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
     install_requires=[
        'dacite',
